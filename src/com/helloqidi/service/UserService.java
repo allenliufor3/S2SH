adfb248b61ab0aa0632164f7d23d49dee1f5da9c
helloqidi <helloqidi@gmail.com>
2014-11-21 10:52:23 +0800

    init project

---
+++ src/com/helloqidi/service/UserService.java
@@ -0,0 +1,20 @@
+package com.helloqidi.service;
+
+import java.util.List;
+
+import com.helloqidi.entity.User;
+
+public interface UserService {
+
+	public void saveUser(User user);
+	
+	public void updateUser(User user);
+	
+	public User findUserById(int id);
+	
+	public void deleteUser(User user);
+	
+	public List<User> findAllList();
+	
+	public User findUserByNameAndPassword(User user);
+}
